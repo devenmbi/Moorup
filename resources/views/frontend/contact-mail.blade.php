28a1142bb579b37c7dc68f4e3ed0246ee36e4dfd
riddhi <riddhi@matrixbricks.com>
2025-02-21 17:46:58 +0530

    Mail functionality for thje detailed page

--- resources/views/frontend/contact-mail.blade.php
+++ resources/views/frontend/contact-mail.blade.php
@@ -31,7 +31,7 @@
         <img src="{{ asset('frontend/assets/images/logo/logo.webp') }}" alt="Murupp" class="logo">
     </div>
 
-    <p>Special Request Submission for <h3> {{ $emailData['product_name'] ?? 'Product' }}</h3></p>
+    <p>Special Request Submission for <h2> {{ $emailData['product_name'] ?? 'Product' }}</h2></p>
     <p><strong>Name:</strong> {{ $emailData['name'] }}</p>
     <p><strong>Email:</strong> {{ $emailData['email'] }}</p>
     <p><strong>Phone:</strong> {{ $emailData['phone'] }}</p>
